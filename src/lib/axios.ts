2398afdcf8fd6e7415bdb5a2116017963eff6fa9
Key Yu Wan <keyflcbyuwan@gmail.com>
2024-01-25 10:40:07 -0300

    feat: loading state on account menu

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // send necessary cookies ("auth" cookie) in the request
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
